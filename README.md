53395a5ef5b21f714417796a8ce577943daf2253
sally20921 <sally20921@snu.ac.kr>
2020-05-24 21:11:35 +0900

    adding README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+##implementation of Moving Semantic Transfer Network
+
+###Besides the 1) standard source classification loss, we also employ 2)domain adversarial loss
+
+
+###Implementation Detail
+- CNN architecture :  
---
+++ model.py
@@ -0,0 +1,11 @@
+'''
+more formally, our total obejective can be written as follows:
+
+    L(X_s, Y_s, X_t) = L_c(X_s, Y_s) + L_dc(X_s, X_t) + L_sm(X_s, Y_s, X_t)
+
+cross entropy loss +  domain adversarial similarity loss + centroid alignment
+
+'''
+
+
+
