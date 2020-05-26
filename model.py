0a263499a7a385619329d8c30efa1360bfb1d425
sally20921 <sally20921@snu.ac.kr>
2020-05-26 15:07:22 +0900

    working on model.py

--- model.py
+++ model.py
@@ -7,5 +7,20 @@ cross entropy loss +  domain adversarial similarity loss + centroid alignment
 
 '''
 
+adversarial_loss = torch.nn.BCELoss()
+classification_loss =  torch.nn.CrossEntropyLoss()
+center_loss = torch.nn.MSELoss(reduction='sum')
 
+def eval_batch(model, x_s, x_t, true_s, true_t, opt, train, args):
+
+     s_clf, s_gen, s_dis = model(x_s)
+     t_clf, t_gen, t_dis = model(x_t)
+
+     #classification loss 
+
+     #generator loss 
+
+     #semantic loss
+
+     loss = C+S*args.lam+G*args.lam
 
